c382c821c79fca4396e4d43b0e0514c50b61024e
va1ker <dead1nside.inside@yandex.ru>
2024-12-16 16:08:27 +0300

    send messages to all users

--- robots/signals.py
+++ robots/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -11,7 +12,12 @@ def robot_post_save_handler(sender, instance, **kwargs) -> None:
     """
     Обработчик сигнала post_save для модели Robot.
     """
-    wishlist_item = Wishlist.objects.filter(robot_serial=instance.serial).first()
-    if wishlist_item:
+    wishlist_items = Wishlist.objects.filter(robot_serial=instance.serial)
+    for wishlist_item in wishlist_items.iterator():
+        email = wishlist_item.customer.email
         model, version = wishlist_item.robot_serial.split("-")
-        send_robot_restock_email.delay(email=wishlist_item.customer.email, model=model, version=version)
+        transaction.on_commit(
+            lambda: send_robot_restock_email.delay(
+                email=email, model=model, version=version
+            )
+        )
