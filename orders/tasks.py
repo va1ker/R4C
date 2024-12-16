dab6730c1214ff989ca0ee62d32d7756219945f8
va1ker <dead1nside.inside@yandex.ru>
2024-12-16 03:10:19 +0300

    project clean up

--- orders/tasks.py
+++ orders/tasks.py
@@ -4,7 +4,6 @@ from django.core.mail import send_mail
 from R4C import settings
 
 
-# TODO: ДОЛжен ли я дропать робота из бд?
 @shared_task
 def send_order_created_email(email: str, serial: str):
     """Заказ создан"""
